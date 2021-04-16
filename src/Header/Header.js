913867d3fee50b24319a84e5c55b7eef60ed0d2b
sagarsubedi <sagarsubediofficial@gmail.com>
2021-04-15 23:21:34 -0400

    changed header a little bit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,21 @@
+// core stuffs
 import React from 'react';
+
+// icons from Material UI
+import PersonIcon from '@material-ui/icons/Person';
+import { IconButton } from '@material-ui/core';
 
 function Header() {
     return (
-        <div>
-            <h1>Test</h1>
+        <div className="header">
+            
+            <IconButton>
+                <PersonIcon
+                    fontSize = "large"
+                    className = "header__icon"
+                />
+            </IconButton>
+
         </div>
     )
 }
